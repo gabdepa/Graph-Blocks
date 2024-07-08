3bdd90078eb531ce38ed9c19b183ae8782863586
Erick.Graeff <erickgraeff@gmail.com>
2024-07-07 21:07:09 -0300

    passando headers para o .c

--- code/rainhas.c
+++ code/rainhas.c
@@ -2,6 +2,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Estrutura para nós do grafo
+struct Node
+{
+  unsigned int v;
+  struct Node *next;
+};
+typedef struct Node Node;
+
+// Estrutura para listas
+struct t_lista
+{
+  Node *ini;
+  unsigned int tamanho;
+};
+typedef struct t_lista t_lista;
+
+/**************************************************BACKTRACKING***************************************************/
+int is_safe(unsigned int *board, unsigned int row, unsigned int col, casa *c, unsigned int k);
+int solve_n_queens_bt(unsigned int *board, unsigned int row, unsigned int n, casa *c, unsigned int k);
+
+/**************************************************CONJUNTOS INDEPENDENTES***************************************************/
+void create_graph(unsigned int *board, t_lista *graph, unsigned int graph_size, unsigned int n);
+void create_available_vertices(unsigned int graph_size, casa *c, unsigned int k, t_lista *available_vertices, unsigned int n);
+void libera_listas(t_lista *graph, unsigned int graph_size, t_lista available_vertices, t_lista independet_set);
+t_lista *ConjIndep(t_lista *graph, unsigned int n, t_lista *independent_set, t_lista *available_vertices);
+
+/**************************************************LISTAS***************************************************/
+int cria_lista(t_lista *l);
+int insere_fim_lista(unsigned int x, t_lista *l);
+int lista_vazia(t_lista *l);
+int insere_inicio_lista(unsigned int x, t_lista *l);
+unsigned int remove_primeiro_lista(unsigned int *item, t_lista *l);
+unsigned int remove_item_lista(unsigned int chave, unsigned int *item, t_lista *l);
+int pertence_lista(unsigned int chave, t_lista *l);
+void destroi_lista(t_lista *l);
+int copia_lista(t_lista *lista1, t_lista *lista2);
+
+
 /**************************************************BACKTRACKING***************************************************/
 // Função para verificar se é seguro colocar uma rainha em uma determinada posição
 int is_safe(unsigned int *board, unsigned int row, unsigned int col, casa *c, unsigned int k)
