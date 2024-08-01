c1c79fe385c86c19a0c45d4cbad276c0257aea4c
gabdepa <gabriel-r-p-p@hotmail.com>
2024-08-01 20:51:56 -0300

    Working for a linear graph

--- trabalho2/src/blocos.c
+++ trabalho2/src/blocos.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include "blocos.h"
 
-// Definições para representar o grafo
 typedef struct AdjListNode
 {
     int dest;
@@ -21,7 +20,6 @@ typedef struct Graph
     AdjList *array;
 } Graph;
 
-// Funções auxiliares para o grafo
 AdjListNode *newAdjListNode(int dest)
 {
     AdjListNode *newNode = (AdjListNode *)malloc(sizeof(AdjListNode));
@@ -50,52 +48,56 @@ void addEdge(Graph *graph, int src, int dest)
     graph->array[dest].head = newNode;
 }
 
-// Função para encontrar os blocos
-void DFS(Graph *graph, int u, bool *visited, int *disc, int *low, int *parent, bool *articulation_point, int *time, int *num_blocos, info_t *blocos, int *edge_count)
+void DFS(Graph *graph, int u, bool *visited, int *disc, int *low, int *parent, bool *articulation_point, int *current_vertices, int *current_edges)
 {
     static int time = 0;
-    int children = 0;
     visited[u] = true;
-    disc[u] = low[u] = ++(*time);
+    disc[u] = low[u] = ++time;
+    (*current_vertices)++;
 
     for (AdjListNode *v = graph->array[u].head; v != NULL; v = v->next)
     {
         int v_id = v->dest;
         if (!visited[v_id])
         {
-            children++;
+            (*current_edges)++;
             parent[v_id] = u;
-            DFS(graph, v_id, visited, disc, low, parent, articulation_point, time, num_blocos, blocos, edge_count);
+            DFS(graph, v_id, visited, disc, low, parent, articulation_point, current_vertices, current_edges);
             low[u] = (low[u] < low[v_id]) ? low[u] : low[v_id];
 
-            if (parent[u] == -1 && children > 1)
+            if ((parent[u] == -1 && (*current_edges > 1)) || (parent[u] != -1 && low[v_id] >= disc[u]))
                 articulation_point[u] = true;
-
-            if (parent[u] != -1 && low[v_id] >= disc[u])
-                articulation_point[u] = true;
-
-            if (low[v_id] > disc[u])
-            {
-                // É uma ponte
-                (*num_blocos)++;
-                blocos[*num_blocos].vertices = children + 1; // aproximado
-                blocos[*num_blocos].arestas = (*edge_count) / 2;
-                *edge_count = 0;
-            }
         }
         else if (v_id != parent[u])
+        {
             low[u] = (low[u] < disc[v_id]) ? low[u] : disc[v_id];
+        }
     }
 }
 
 info_t *info_blocos(unsigned int *num_blocos)
 {
     int V, src, dest;
-    scanf("%d", &V);
+    if (scanf("%d", &V) != 1)
+    {
+        printf("Erro ao ler o número de vértices.\n");
+        return NULL;
+    }
 
     Graph *graph = createGraph(V);
-    while (scanf("%d %d", &src, &dest) != EOF)
+    if (!graph)
     {
+        printf("Erro ao criar o grafo.\n");
+        return NULL;
+    }
+
+    while (scanf("%d %d", &src, &dest) == 2)
+    {
+        if (src < 1 || src > V || dest < 1 || dest > V)
+        {
+            printf("Aresta inválida: %d %d\n", src, dest);
+            continue;
+        }
         addEdge(graph, src - 1, dest - 1);
     }
 
@@ -104,8 +106,6 @@ info_t *info_blocos(unsigned int *num_blocos)
     int *low = (int *)malloc(V * sizeof(int));
     int *parent = (int *)malloc(V * sizeof(int));
     bool *articulation_point = (bool *)malloc(V * sizeof(bool));
-    int *edge_count = (int *)malloc(sizeof(int));
-    *edge_count = 0;
 
     for (int i = 0; i < V; i++)
     {
@@ -119,9 +119,17 @@ info_t *info_blocos(unsigned int *num_blocos)
 
     for (int i = 0; i < V; i++)
     {
-        if (visited[i] == false)
+        if (!visited[i])
         {
-            DFS(graph, i, visited, disc, low, parent, articulation_point, time, num_blocos, blocos, edge_count);
+            int current_vertices = 0;
+            int current_edges = 0;
+            DFS(graph, i, visited, disc, low, parent, articulation_point, &current_vertices, &current_edges);
+            if (current_vertices > 0)
+            {
+                blocos[*num_blocos].vertices = current_vertices;
+                blocos[*num_blocos].arestas = current_edges; // Contar corretamente as arestas
+                (*num_blocos)++;
+            }
         }
     }
 
@@ -131,7 +139,6 @@ info_t *info_blocos(unsigned int *num_blocos)
     free(low);
     free(parent);
     free(articulation_point);
-    free(edge_count);
 
     return blocos;
 }
