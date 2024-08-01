88bde62e46fe10c79bb350a8b936cc176797a2bd
gabdepa <gabriel-r-p-p@hotmail.com>
2024-08-01 11:22:26 -0300

    Starting trabalho2

--- trabalho1/code/teste.c
+++ trabalho1/code/teste.c
@@ -44,7 +44,7 @@ static casa *proibe_diagonais(unsigned int n, casa *proibido)
 
 int main(void)
 {
-  unsigned int n = 8;
+  unsigned int n = 23;
   unsigned int *resposta = malloc(n * sizeof(unsigned int));
   unsigned int k = 2 * n;
   casa *proibido = malloc(k * sizeof(casa));
